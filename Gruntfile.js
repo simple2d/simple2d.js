c62a02dabe4575b32d54d6f60c3f89f155f061b0
Tom Black <blacktm@gmail.com>
2016-10-31 00:52:36 -0400

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+  
+  require('google-closure-compiler').grunt(grunt);
+  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: '\n\n'
+      },
+      dist: {
+        src: [
+          'src/start.js',
+          'src/simple2d.js',
+          'src/shapes.js',
+          'src/image.js',
+          'src/sprite.js',
+          'src/text.js',
+          'src/sound.js',
+          'src/music.js',
+          'src/window.js',
+          'src/gl.js',
+          'src/end.js'
+        ],
+        dest: 'build/simple2d.js'
+      }
+    },
+    eslint: {
+      dist: ['build/simple2d.js']
+    },
+    'closure-compiler': {
+      dist: {
+        files: {
+          'build/simple2d.min.js': ['build/simple2d.js']
+        },
+        options: {
+          compilation_level: 'SIMPLE'
+          // ,formatting: 'pretty_print'
+        }
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  
+  // Define tasks
+  grunt.registerTask('default', [
+    'concat',
+    'eslint',
+    'closure-compiler'
+  ]);
+};
