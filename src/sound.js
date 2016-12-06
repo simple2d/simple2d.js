9f4cb58dde73e1c77acb5a3e607beec27c571807
Tom Black <blacktm@gmail.com>
2016-12-05 23:50:45 -0500

    Add sound

--- src/sound.js
+++ src/sound.js
@@ -3,10 +3,20 @@
 /*
  * Create a sound, given an audio file path
  */
-S2D.CreateSound = function(path) {};
+S2D.CreateSound = function(path) {
+  
+  // TODO: Check if image file exists
+  
+  var sound = Object.create(S2D.Sound);
+  sound.data = new Audio(path);
+  
+  return sound;
+};
 
 
 /*
  * Play the sound
  */
-S2D.PlaySound = function(sound) {};
+S2D.PlaySound = function(sound) {
+  sound.data.play();
+};
