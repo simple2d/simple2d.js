02892fcd78e596aaf60e87cc17b074b2aafb1625
Tom Black <blacktm@gmail.com>
2017-05-16 17:14:34 -0400

    Add lines, courtesy of @KillaPL
    Adds `S2D.DrawLine` and tests

--- src/shapes.js
+++ src/shapes.js
@@ -33,3 +33,26 @@ S2D.DrawQuad = function(x1,  y1,
                       x4, y4, c4r, c4g, c4b, c4a,
                       x1, y1, c1r, c1g, c1b, c1a);
 };
+
+
+/*
+ * Draw a line from a quad
+ */
+S2D.DrawLine = function(x1,  y1,  x2,  y2,
+                        width,
+                        c1r, c1g, c1b, c1a,
+                        c2r, c2g, c2b, c2a,
+                        c3r, c3g, c3b, c3a,
+                        c4r, c4g, c4b, c4a) {
+  
+  var length = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+  var x = ((x2 - x1) / length) * width / 2;
+  var y = ((y2 - y1) / length) * width / 2;
+  
+  S2D.DrawQuad(
+    x1 - y, y1 + x, c1r, c1g, c1b, c1a,
+    x1 + y, y1 - x, c2r, c2g, c2b, c2a,
+    x2 + y, y2 - x, c3r, c3g, c3b, c3a,
+    x2 - y, y2 + x, c4r, c4g, c4b, c4a
+  );
+};
